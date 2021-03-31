0e169a926e7d537b96cc805f19e1e8f68be2b046
SoyaNyan <nyan@soya.moe>
2021-04-01 00:52:26 +0900

    dataset saving function added

--- data-collecting/script.js
+++ data-collecting/script.js
@@ -12,3 +12,31 @@ $(document).on("click", "#capture-start", function () {
 $(document).on("change", "input[name=truthLabel]", function () {
     truthLabel = parseInt($("input[name=truthLabel]:checked").val());
 });
+
+let featureData = [];
+let labelData = [];
+$(document).on("click", "#load-datasets", function () {
+    featureData = JSON.parse(localStorage.getItem("featureData"));
+    if (featureData === null) {
+        featureData = [];
+    }
+
+    labelData = JSON.parse(localStorage.getItem("labelData"));
+    if (labelData === null) {
+        labelData = [];
+    }
+
+    // update datsets info
+    $("#feature-info").text(featureData.length + " Features");
+    $("#label-info").text(labelData.length + " Labels");
+});
+
+$(document).on("click", "#save-datasets", function () {
+    let featureString = JSON.stringify(featureData);
+    let labelString = JSON.stringify(labelData);
+
+    let featureWindow = window.open();
+    featureWindow.document.write(featureString);
+    let labelWindow = window.open();
+    labelWindow.document.write(labelString);
+});
