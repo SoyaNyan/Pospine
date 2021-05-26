c8b33474f115fa295b4c723bffeedeb04de43a9a
yewl1110 <38202551+yewl1110@users.noreply.github.com>
2021-05-26 17:36:16 +0900

    demo

---
+++ demo/assets/js/script.js
@@ -0,0 +1,202 @@
+let chart;
+
+$(document).ready(function () {
+	$(".nav-link").on("click", function () {
+		const navMenu = $(this).attr("id");
+
+		if (navMenu === "nav-statistics") {
+			$("#nav-home").removeClass("active").removeProp("aria-current");
+			$("#nav-statistics").addClass("active").prop("aria-current", "page");
+			if ($("#section-main").css("display") !== "none") {
+				$("#section-main").slideUp(1000);
+			}
+			if ($("#section-start").css("display") !== "none") {
+				$("#section-start").slideUp(1000);
+			}
+			if ($("#section-statistics").css("display") === "none") {
+				$("#section-statistics").slideDown(1000);
+			}
+
+			reloadChart();
+		} else {
+			$("#nav-statistics").removeClass("active").removeProp("aria-current");
+			$("#nav-home").addClass("active").prop("aria-current", "page");
+			if ($("#section-main").css("display") === "none") {
+				$("#section-main").slideDown(1000);
+			}
+			if ($("#section-start").css("display") !== "none") {
+				$("#section-start").slideUp(1000);
+			}
+			if ($("#section-statistics").css("display") !== "none") {
+				$("#section-statistics").slideUp(1000);
+			}
+		}
+	});
+
+	$("#start").on("click", function () {
+		$("#section-main").slideUp(1000);
+		$("#section-start").slideDown(1000);
+		state = true;
+	});
+
+	$("#stop").on("click", function () {
+		$("#section-main").slideDown(1000);
+		$("#section-start").slideUp(1000);
+		state = false;
+	});
+
+	reloadChart();
+});
+
+const loadStatistics = () => {
+	let storedData = JSON.parse(localStorage.getItem("statistics"));
+	if (storedData === null) {
+		storedData = {
+			totalPose: 0,
+			goodPose: 0,
+			badPose: 0,
+		};
+	}
+
+	return storedData;
+};
+
+const saveStatistics = (data) => {
+	let storedData = loadStatistics();
+
+	if (data.poseClass) {
+		storedData.goodPose++;
+	} else {
+		storedData.badPose++;
+	}
+	storedData.totalPose++;
+
+	localStorage.setItem("statistics", JSON.stringify(storedData));
+};
+
+const resetStatistics = () => {
+	let data = {
+		totalPose: 0,
+		goodPose: 0,
+		badPose: 0,
+	};
+
+	localStorage.setItem("statistics", JSON.stringify(data));
+};
+
+const updateStatistics = () => {
+	// update progress bar
+	updateGoodProgress();
+	updateBadProgress();
+
+	// update apex chart data
+	chart.updateSeries([
+		{
+			data: predictionArray,
+		},
+	]);
+};
+
+const updateGoodProgress = () => {
+	const storedData = loadStatistics();
+
+	const progress = $("#good-progress");
+	const ratio = (storedData.goodPose / storedData.totalPose) * 100;
+	if (!isNaN(ratio)) {
+		progress.css("width", `${ratio.toFixed(0)}%`);
+		progress.attr("aria-valuenow", ratio.toFixed(0));
+
+		$("#good-percent").text(`${ratio.toFixed(2)}%`);
+	} else {
+		progress.css("width", `0%`);
+		progress.attr("aria-valuenow", 0);
+
+		$("#good-percent").text(`0%`);
+	}
+};
+
+const updateBadProgress = () => {
+	const storedData = loadStatistics();
+
+	const progress = $("#bad-progress");
+	const ratio = (storedData.badPose / storedData.totalPose) * 100;
+	if (!isNaN(ratio)) {
+		progress.css("width", `${ratio.toFixed(0)}%`);
+		progress.attr("aria-valuenow", ratio.toFixed(0));
+
+		$("#bad-percent").text(`${ratio.toFixed(2)}%`);
+	} else {
+		progress.css("width", `0%`);
+		progress.attr("aria-valuenow", 0);
+
+		$("#bad-percent").text(`0%`);
+	}
+};
+
+const getDetectMode = () => {
+	return $("#detect-mode").val();
+};
+
+const reloadChart = () => {
+	// apex chart
+	const options = {
+		series: [
+			{
+				name: "Score",
+				data: predictionArray.slice(),
+			},
+		],
+		chart: {
+			height: 350,
+			type: "line",
+		},
+		stroke: {
+			width: 7,
+			curve: "smooth",
+		},
+		xaxis: {
+			type: "category",
+			categories: ["#1", "#2", "#3", "#4", "#5", "#6", "#7", "#8", "#9", "#10"],
+			tickAmount: 10,
+		},
+		title: {
+			text: "Posture History",
+			align: "left",
+			style: {
+				fontSize: "16px",
+				color: "#666",
+			},
+		},
+		fill: {
+			type: "gradient",
+			gradient: {
+				shade: "dark",
+				gradientToColors: ["#FDD835"],
+				shadeIntensity: 1,
+				type: "horizontal",
+				opacityFrom: 1,
+				opacityTo: 1,
+				stops: [0, 100, 100, 100],
+			},
+		},
+		markers: {
+			size: 4,
+			colors: ["#FFA41B"],
+			strokeColors: "#fff",
+			strokeWidth: 2,
+			hover: {
+				size: 7,
+			},
+		},
+		yaxis: {
+			min: 0,
+			max: 1,
+			title: {
+				text: "Score",
+			},
+		},
+	};
+
+	chart = new ApexCharts(document.querySelector("#chart"), options);
+	chart.render();
+};
---
+++ demo/assets/js/sketch.js
@@ -0,0 +1,200 @@
+// globals
+let net, pospine;
+let poses = [];
+let state = false;
+let predictionArray = [];
+
+// options
+const poseNetOptions = {
+	modelSize: 0.75,
+	outputStride: 16,
+	flipHorizontal: false,
+};
+
+// video & canvas elements
+let video = document.getElementById("video");
+let canvas = document.getElementById("video-canvas");
+let ctx = canvas.getContext("2d");
+
+// load webcam from browser
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+	navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+		video.srcObject = stream;
+		video.play();
+	});
+}
+
+// app setup(p5js)
+async function setup() {
+	predictionInit();
+
+	// load posenet by downloading the weights for the model.
+	video.addEventListener("loadeddata", function () {
+		posenet
+			.load({
+				architecture: "MobileNetV1",
+				outputStride: poseNetOptions.outputStride,
+				multiplier: poseNetOptions.modelSize,
+			})
+			.then(function (loadedNet) {
+				net = loadedNet;
+				// when it's loaded, start estimating poses
+				requestAnimationFrame(function () {
+					estimatePoses();
+				});
+			});
+	});
+
+	// load pospine model
+	inportModel(pospineModel);
+	pospine = await tf.loadLayersModel("localstorage://pospine");
+}
+
+// PoseNet Single-pose Estimation
+function estimatePoses() {
+	// call posenet to estimate a pose
+	net.estimateSinglePose(video, poseNetOptions.flipHorizontal).then(function (pose) {
+		// store the keypoints from the pose to draw it below
+		poses = [
+			{
+				pose: pose,
+			},
+		];
+
+		if (state) {
+			setInterval(function () {
+				let x = normalizeData(processData(poses));
+				let xs = tf.tensor2d(x, [1, 34]);
+
+				let pred = pospine.predict(xs);
+				let score = 0;
+				pred.data().then((data) => {
+					score = data[0].toFixed(3);
+					checkPosture(score, getDetectMode());
+
+					// for statistics
+					if (score >= 0.5) {
+						saveStatistics({
+							poseClass: true,
+						});
+					} else {
+						saveStatistics({
+							poseClass: false,
+						});
+					}
+
+					updateStatistics();
+				});
+			}, 1000);
+		}
+
+		// next animation loop, call posenet again to estimate poses
+		requestAnimationFrame(function () {
+			// Draw the video element into the canvas
+			ctx.drawImage(video, 0, 0, 640, 480);
+			// We can call both functions to draw all keypoints and the skeletons
+			drawKeypoints();
+			// Loop over the drawCameraIntoCanvas function
+			estimatePoses();
+		});
+	});
+}
+
+// process input
+function processData(data) {
+	let pose = data[0].pose;
+	let keyPoints = pose.keypoints;
+	let tmpArray = [];
+
+	keyPoints.forEach((point) => {
+		tmpArray.push(point.position.x);
+		tmpArray.push(point.position.y);
+	});
+
+	return tmpArray;
+}
+
+// normalize input
+function normalizeData(data) {
+	return data.map((e, i) => {
+		return (e - normalizeInfo.min) / (normalizeInfo.max - normalizeInfo.min);
+	});
+}
+
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints() {
+	// Loop through all the poses detected
+	for (let i = 0; i < poses.length; i += 1) {
+		// For each pose detected, loop through all the keypoints
+		for (let j = 0; j < poses[i].pose.keypoints.length; j += 1) {
+			let keypoint = poses[i].pose.keypoints[j];
+			// Only draw an ellipse is the pose probability is bigger than 0.2
+			if (keypoint.score > 0.2) {
+				ctx.beginPath();
+				ctx.arc(keypoint.position.x, keypoint.position.y, 10, 0, 2 * Math.PI);
+				ctx.stroke();
+			}
+		}
+	}
+}
+
+// array init
+const predictionInit = () => {
+	predictionArray = [];
+};
+
+// array push(enqueue)
+const pushPrediction = (predScore) => {
+	if (predictionArray.length < 10) {
+		predictionArray.push(predScore);
+	} else {
+		shiftPrediction();
+		predictionArray.push(predScore);
+	}
+
+	return predictionArray.length;
+};
+
+// array shift(dequeue)
+const shiftPrediction = () => {
+	return predictionArray.shift();
+};
+
+const checkPosture = (predScore, mode) => {
+	pushPrediction(predScore);
+
+	// prediction array ready
+	if (predictionArray.length >= 10) {
+		let incorrectPoseArray = predictionArray.filter((s) => {
+			return s < 0.5;
+		});
+
+		let limit = 0;
+		switch (mode) {
+			case 0: // high sensitivity
+				limit = 2;
+				break;
+			case 1: // medium sensitivity(default)
+				limit = 5;
+				break;
+			case 2: // low sensitivity
+				limit = 8;
+				break;
+			default:
+				limit = 5;
+		}
+
+		// check incorrect posture ratio
+		if (incorrectPoseArray.length >= limit) {
+			let notification = {
+				title: "Pospine Notification",
+				options: {
+					body: "You're in a bad posture!! Sit up straight.",
+					icon: "logo.png",
+				},
+			};
+
+			notify(notification);
+		}
+	}
+};
